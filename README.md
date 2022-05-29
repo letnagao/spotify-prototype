df9a7c1e7a99989e47956d58601a1719b35bddba
letnagao <leticia-nagao@hotmail.com>
2022-05-28 21:45:46 -0300

    finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# spotify-prototype
+## Spotify Prototype ✨
+Javascript Project
+
+## Technologies used 🛠️
+**Design**: *CSS3 & HTML*<br />
+
+## Usage 📋
+<details open>
+<summary>1. Server Setup</summary>
+
+```bash
+#1. clone this project
+~ git clone https://github.com/letnagao/spotify-prototype.git
+#2. cd into it
+~ cd  spotify-prototype
+#3. start the project from index.html
+```
+</details>
+
+## Contributing 💡
+Pull request are welcome but please open an issue and discuss what you will do before 😊
+
+## License 📄
+This project is open-sourced under the [MIT license](https://opensource.org/licenses/MIT).
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const songImage = document.getElementById("song-image");
 const songName = document.getElementById("song-name");
 const songArtist = document.getElementById("song-artist");
 const pauseButton = document.getElementById("pause");
-const playButton = document.getElementById("play")
+const playButton = document.getElementById("play");
 const playlistButton = document.getElementById("playlist");
 const maxDuration = document.getElementById("max-duration");
 const currentTimeRef = document.getElementById("current-time");
@@ -21,34 +21,210 @@ let index;
 
 let loop = true;
 
-const songsList = [{
+const songsList = [
+  {
     name: "Make Me Move",
     link: "make-me-move.mp3",
     artist: "Culture Code",
     image: "make-me-move.jpg",
-},
-{
+  },
+  {
     name: "Where We Started",
     link: "where-we-started.mp3",
     artist: "Lost Sky",
     image: "where-we-started.jpg",
-}, 
-{
+  },
+  {
     name: "On & On",
     link: "on-on.mp3",
     artist: "Cartoon",
     image: "on-on.jpg",
-},
-{
+  },
+  {
     name: "Throne",
     link: "throne.mp3",
     artist: "Rival",
     image: "throne.jpg",
-},
-{
+  },
+  {
     name: "Need You Now",
     link: "need-you-now.mp3",
     artist: "Venemy",
     image: "need-you-now.jpg",
-},
-]
+  },
+];
+
+let events = {
+  mouse: {
+    click: "click",
+  },
+  touch: {
+    click: "touchstart",
+  },
+};
+
+let deviceType = "";
+
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+const timeFormatter = (timeInput) => {
+  let minute = Math.floor(timeInput / 60);
+  minute = minute < 10 ? "0" + minute : minute;
+  let second = Math.floor(timeInput % 60);
+  second = second < 10 ? "0" + second : second;
+  return `${minute}:${second}`;
+};
+
+const setSong = (arrayIndex) => {
+  let { name, link, artist, image } = songsList[arrayIndex];
+  audio.src = link;
+  songName.innerHTML = name;
+  songArtist.innerHTML = artist;
+  songImage.src = image;
+  audio.onloadedmetadata = () => {
+    maxDuration.innerText = timeFormatter(audio.duration);
+  };
+};
+
+const playAudio = () => {
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+};
+
+repeatButton.addEventListener("click", () => {
+  if (repeatButton.classList.contains("active")) {
+    repeatButton.classList.remove("active");
+    audio.loop = false;
+    console.log("repeat off");
+  } else {
+    repeatButton.classList.add("active");
+    audio.loop = true;
+    console.log("repeat on");
+  }
+});
+
+const nextSong = () => {
+  if (loop) {
+    if (index == songsList.length - 1) {
+      index = 0;
+    } else {
+      index += 1;
+    }
+    setSong(index);
+
+    playAudio();
+  } else {
+    let randIndex = Math.floor(Math.random() * songsList.length);
+    console.log(randIndex);
+    setSong(randIndex);
+    playAudio();
+  }
+};
+
+const pauseAudio = () => {
+  audio.pause();
+  pauseButton.classList.add("hide");
+  playButton.classList.remove("hide");
+};
+
+const previousSong = () => {
+  if (index > 0) {
+    pauseAudio();
+    index -= 1;
+  } else {
+    index = songsList.length - 1;
+  }
+  setSong(index);
+  playAudio();
+};
+
+audio.onended = () => {
+  nextSong();
+};
+
+shuffleButton.addEventListener("click", () => {
+  if (shuffleButton.classList.contains("active")) {
+    shuffleButton.classList.remove("active");
+    loop = true;
+    console.log("shuffle off");
+  } else {
+    shuffleButton.classList.add("active");
+    loop = false;
+    console.log("shuffle on");
+  }
+});
+
+playButton.addEventListener("click", playAudio);
+
+nextButton.addEventListener("click", nextSong);
+
+pauseButton.addEventListener("click", pauseAudio);
+
+prevButton.addEventListener("click", previousSong);
+
+isTouchDevice();
+progressBar.addEventListener(events[deviceType].click, (event) => {
+  let coordStart = progressBar.getBoundingClientRect().left;
+  let coordEnd = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
+  let progress = (coordEnd - coordStart) / progressBar.offsetWidth;
+
+  currentProgress.style.width = progress * 100 + "%";
+
+  audio.currentTime = progress * audio.duration;
+
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+});
+
+setInterval(() => {
+  currentTimeRef.innerHTML = timeFormatter(audio.currentTime);
+  currentProgress.style.width =
+    (audio.currentTime / audio.duration.toFixed(3)) * 100 + "%";
+});
+
+audio.addEventListener("timeupdate", () => {
+  currentTimeRef.innerText = timeFormatter(audio.currentTime);
+});
+
+const initializePlaylist = () => {
+  for (let i in songsList) {
+    playlistSongs.innerHTML += `<li class='playlistSong' onclick='setSong(${i})'>
+            <div class="playlist-image-container">
+                <img src="${songsList[i].image}"/>
+            </div>
+            <div class="playlist-song-details">
+                <span id="playlist-song-name">
+                    ${songsList[i].name}
+                </span>
+                <span id="playlist-song-artist-album">
+                    ${songsList[i].artist}
+                </span>
+            </div>
+        </li>`;
+  }
+};
+
+playlistButton.addEventListener("click", () => {
+  playlistContainer.classList.remove("hide");
+});
+
+closeButton.addEventListener("click", () => {
+  playlistContainer.classList.add("hide");
+});
+
+window.onload = () => {
+  index = 0;
+  setSong(index);
+  initializePlaylist();
+};
